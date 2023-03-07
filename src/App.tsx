235d6ae5d7e2ff6aac60c3d78c91d31b360e641c
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-07 14:19:35 +0800

    create basic app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './css/App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Home } from './pages/Home';
+import { NotFoundPage } from './pages/Error';
+
+function App() {
+  return (
+    <div className="App">
+      <Router basename="/Homework-05-Image-Zoom-In-Out">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
