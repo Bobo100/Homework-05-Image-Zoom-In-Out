1395e7712db1288e034cfe111104b4117ab7b6bc
Bobo100 <w150lione@gmail.com>
2023-03-09 01:05:49 +0800

    更新

--- src/pages/component/CanvasAdv.tsx
+++ src/pages/component/CanvasAdv.tsx
@@ -8,10 +8,18 @@ export default function CanvasAdv(props: { src: string }) {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [scaleFactor, setScaleFactor] = useState(1);
-    const [translate, setTranslate] = useState({ x: 0, y: 0 });
+
+    // 紀錄滑鼠的位置
+    const [mousepos, setMousepos] = useState({ x: 0, y: 0 });
+
+    // 紀錄滑鼠在圖片的位置
+    const [mouseposInImage, setMouseposInImage] = useState({ x: 0, y: 0 });
+
+    //紀錄圖片的大小
+    const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
 
     const maxScaleFactor = 3;
-    const minScaleFactor = 1;
+    const minScaleFactor = 1.1;
     // 設定小數點位數
     Decimal.set({ precision: 10 });
 
@@ -32,10 +40,13 @@ export default function CanvasAdv(props: { src: string }) {
             canvas.width = img.width;
             canvas.height = img.height;
 
+            setImageSize({ width: img.width, height: img.height });
+
             const centerX = canvas.width / 2;
             const centerY = canvas.height / 2;
-            ctx.translate(centerX + translate.x, centerY + translate.y);
+            ctx.translate(centerX, centerY);
             ctx.scale(scaleFactor, scaleFactor);
+
             // 繪製圖像
             ctx.drawImage(img, -img.width / 2, -img.height / 2);
         };
@@ -47,6 +58,7 @@ export default function CanvasAdv(props: { src: string }) {
                 newScaleFactor = new Decimal(scaleFactor).plus(delta * 0.1).toNumber();
             }
 
+<<<<<<< Updated upstream
             /* 這裡是重點
             1. 我們需要獲取滑鼠在畫布上的位置
             2. 我們要知道滑鼠在畫布上的位置在縮放後的位置 (縮放的value {newScaleFactor})
@@ -57,11 +69,42 @@ export default function CanvasAdv(props: { src: string }) {
             const rect = canvas.getBoundingClientRect();
             const mouseX = e.clientX - rect.left;
             const mouseY = e.clientY - rect.top
+=======
+            // 紀錄滑鼠的位置
+            const rect = canvas.getBoundingClientRect();
+
+            const scaleX = canvas.width / rect.width;
+            const scaleY = canvas.height / rect.height;
+
+            const x = e.clientX;
+            const y = e.clientY;
+>>>>>>> Stashed changes
+
+            const centerX = canvas.width / 2;
+            const centerY = canvas.height / 2;
+
+            // 滑鼠在 Canvas 元素上的坐标
+            const canvasX = (x - rect.left) * scaleX;
+            const canvasY = (y - rect.top) * scaleY;
 
 
+            // 考虑到缩放比例和 translate 平移影响后的坐标
+
+            // 滑鼠在 Canvas 元素上的坐标
+            const mouseX = (canvasX - centerX)
+            const mouseY = (canvasY - centerY)
+
+            setMousepos({ x: mouseX, y: mouseY });
+
+<<<<<<< Updated upstream
             setScaleFactor(newScaleFactor);
 
 
+=======
+            if (newScaleFactor !== scaleFactor) {
+                setScaleFactor(newScaleFactor);
+            }
+>>>>>>> Stashed changes
         };
 
         canvas.addEventListener("wheel", handleWheel);
@@ -70,7 +113,7 @@ export default function CanvasAdv(props: { src: string }) {
             canvas.removeEventListener("wheel", handleWheel);
         };
 
-    }, [props.src, scaleFactor, translate.x, translate.y]);
+    }, [props.src, scaleFactor]);
 
     const handleZoomIn = () => {
         if (scaleFactor < maxScaleFactor) {
@@ -90,6 +133,21 @@ export default function CanvasAdv(props: { src: string }) {
 
     return (
         <div className="margin">
+            <div className="flex center">
+                滑鼠在畫布的位置
+                <div className="margin">X: {mousepos.x}</div>
+                <div className="margin">Y: {mousepos.y}</div>
+            </div>
+            <div className="flex center">
+                滑鼠在圖片的位置
+                <div className="margin">X: {mouseposInImage.x}</div>
+                <div className="margin">Y: {mouseposInImage.y}</div>
+            </div>
+            <div className="flex center">
+                圖片的大小
+                <div className="margin">Width: {imageSize.width}</div>
+                <div className="margin">Height: {imageSize.height}</div>
+            </div>
             <div className="relative">
                 <canvas ref={canvasRef} />
                 <Scrollbar className="vertical" min={minScaleFactor} max={maxScaleFactor} step={0.1} value={scaleFactor} onChange={handleChange} />
